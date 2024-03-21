42ea7a9d32ace0f25aba0d3c602a20d777f0bd69
Bogdan Kitaev <6ogdashaa@gmail.com>
2024-03-21 12:33:40 +0300

    refactor: minor changes

--- lab2/build.gradle.kts
+++ lab2/build.gradle.kts
@@ -13,6 +13,10 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("org.hibernate.orm:hibernate-core:6.4.4.Final")
+    implementation("org.postgresql:postgresql:42.7.2")
+    implementation("jakarta.transaction:jakarta.transaction-api")
+
+
 }
 
 tasks.test {
