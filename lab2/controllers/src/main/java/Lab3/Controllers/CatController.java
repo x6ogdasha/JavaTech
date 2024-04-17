ec2fbd1a7cb0c719443b2e482202735403f53dcb
Bogdan Kitaev <6ogdashaa@gmail.com>
2024-04-17 20:54:05 +0300

    refactor: added CRUD methods

--- lab2/controllers/src/main/java/Lab3/Controllers/CatController.java
+++ lab2/controllers/src/main/java/Lab3/Controllers/CatController.java
@@ -1,12 +1,17 @@
 package Lab3.Controllers;
 
+import Lab3.Dto.CatDto;
 import Lab3.Entities.Cat;
 import Lab3.Services.CatService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 @RestController
+@RequestMapping("/cat")
 public class CatController {
     private final CatService catService;
 
@@ -15,8 +20,30 @@ public class CatController {
         this.catService = catService;
     }
 
-    @GetMapping("/")
-    public String test(){
-        return "Hello";
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<CatDto> getCat(@RequestParam Long id) {
+
+        CatDto cat = catService.findCat(id);
+        return ResponseEntity.ok(cat);
+    }
+    @PostMapping
+    public ResponseEntity<String> addCat(@RequestBody CatDto cat) {
+
+        catService.saveCat(cat);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Cat added successfully");
+    }
+
+    @PutMapping
+    public ResponseEntity<String> modifyCat(@RequestBody CatDto cat) {
+
+        catService.updateCat(cat);
+        return ResponseEntity.ok("Cat modified successfully");
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> deleteCat(@RequestParam Long id) {
+
+        catService.deleteCat(id);
+        return ResponseEntity.ok("Cat deleted successfully");
     }
 }
--- lab2/controllers/src/main/java/Lab3/Controllers/OwnerController.java
+++ lab2/controllers/src/main/java/Lab3/Controllers/OwnerController.java
@@ -1,14 +1,17 @@
 package Lab3.Controllers;
 
+import Lab3.Dto.OwnerDto;
 import Lab3.Entities.Owner;
 import Lab3.Services.OwnerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/owner")
 public class OwnerController {
 
     private final OwnerService ownerService;
@@ -17,11 +20,30 @@ public class OwnerController {
     public OwnerController(OwnerService ownerService) {
         this.ownerService = ownerService;
     }
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<OwnerDto> getOwner(@RequestParam Long id) {
 
-    @GetMapping("/owners")
-    public List<Owner> getAllOwners() {
+        OwnerDto owner = ownerService.findOwner(id);
+        return ResponseEntity.ok(owner);
+    }
 
-        return ownerService.getAllOwners();
+    @PostMapping
+    public void addHost(@RequestBody OwnerDto owner) {
+
+        ownerService.saveOwner(owner);
+    }
+
+    @PutMapping
+    public void modifyHost(@PathVariable String name, @RequestBody OwnerDto owner) {
+
+        owner.setName(name);
+        ownerService.updateOwner(owner);
+    }
+
+    @DeleteMapping
+    public void deleteHost(@PathVariable Long id) {
+
+        ownerService.deleteOwner(id);
     }
 
 }
